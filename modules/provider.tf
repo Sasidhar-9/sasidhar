3fdd773d8783a74a85e08169751b5c99b84a0d00
sasidhar <sasidharulava2@gmail.com>
2024-09-25 11:25:40 +0530

    sasidhar

--- modules/provider.tf
+++ modules/provider.tf
@@ -10,7 +10,6 @@ terraform {
 #Configure the AWS Provider
 provider "aws" {
     region = "ap-south-1"
-    access_key = "AKIAQ3EGWHOLCKFJ3GFO"
-    secret_key = "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"  
+  
     
 }
